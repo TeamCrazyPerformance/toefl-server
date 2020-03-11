4c9be6831e159b66175f8f2b28aea4ac9ee7984b
eunsooJeon <ensoojn@gmail.com>
2020-03-11 21:30:19 +0900

    init

---
+++ src/main/java/com/tcp/toeflserver/ToeflServerApplication.java
@@ -0,0 +1,13 @@
+package com.tcp.toeflserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToeflServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToeflServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcp/toeflserver/ToeflServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcp.toeflserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToeflServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
