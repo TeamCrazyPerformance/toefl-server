271dbee65438048bfd1e4f2dee72e1bfff624112
Kwoo <dlrjsdn4357@gmail.com>
2020-04-09 18:56:34 +0900

    #24 success 응답이 boolean 형으로 가도록 변경. 예외 메세지는 분리

--- src/main/java/com/tcp/toeflserver/review/ReviewController.java
+++ src/main/java/com/tcp/toeflserver/review/ReviewController.java
@@ -3,7 +3,9 @@ package com.tcp.toeflserver.review;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
@@ -13,46 +15,50 @@ public class ReviewController {
     private final ReviewService reviewService;
 
     @GetMapping("/place")
-    public ReviewApiResponse getReviewByPlace(@RequestParam SelectReview selectReview){
+    public ReviewApiResponse getReviewByPlace(SelectReview selectReview) {
         List<Review> reviews = reviewService.getReviewsByPlace(selectReview);
 
         ReviewApiResponse response = ReviewApiResponse.builder()
-                .reviewList(reviews)
+                .success(reviews != null)
+                .reviewList(Optional.ofNullable(reviews).orElse(new ArrayList<>()))
                 .build();
 
         return response;
     }
 
     @GetMapping("/user")
-    public ReviewApiResponse getReviewsByUser(@RequestParam SelectReview selectReview){
+    public ReviewApiResponse getReviewsByUser(SelectReview selectReview) {
         List<Review> reviews = reviewService.getReviewsByUser(selectReview);
 
         ReviewApiResponse response = ReviewApiResponse.builder()
-                .reviewList(reviews)
+                .success(reviews != null)
+                .reviewList(Optional.ofNullable(reviews).orElse(new ArrayList<>()))
                 .build();
 
         return response;
     }
 
     @PostMapping
-    public ReviewApiResponse addReview(@RequestBody Review review){
-        Review addReview = new Review(review);
-        String added = reviewService.addReview(addReview);
+    public ReviewApiResponse addReview(@RequestBody Review review) {
+        String added = reviewService.addReview(review);
+        boolean success = added.equals("Success");
 
         ReviewApiResponse response = ReviewApiResponse.builder()
-                .success(added)
+                .success(success)
+                .errMsg(success ? null : added)
                 .build();
 
         return response;
     }
 
     @DeleteMapping("/{reviewId}")
-    public ReviewApiResponse deleteReview(@PathVariable String reviewId){
-        int index = Integer.parseInt(reviewId);
-        String deleted = reviewService.removeReview(index);
+    public ReviewApiResponse deleteReview(@PathVariable String reviewId) {
+        String deleted = reviewService.removeReview(Integer.parseInt(reviewId));
+        boolean success = deleted.equals("Success");
 
         ReviewApiResponse response = ReviewApiResponse.builder()
-                .success(deleted)
+                .success(success)
+                .errMsg(success ? null : deleted)
                 .build();
 
         return response;
--- src/main/java/com/tcp/toeflserver/review/ReviewService.java
+++ src/main/java/com/tcp/toeflserver/review/ReviewService.java
@@ -1,43 +1,45 @@
 package com.tcp.toeflserver.review;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class ReviewService {
-    @Autowired
-    private ReviewRepository reviewRepository;
+    private final ReviewRepository reviewRepository;
 
-
-    public List<Review> getReviewsByUser(SelectReview selectReview){
+    List<Review> getReviewsByUser(SelectReview selectReview){
+        selectReview.setPage(selectReview.getPage()*20);
         return reviewRepository.selectReviewsByUser(selectReview);
     }
 
-    public List<Review> getReviewsByPlace(SelectReview selectReview){
+    List<Review> getReviewsByPlace(SelectReview selectReview){
+        selectReview.setPage(selectReview.getPage()*20);
         return reviewRepository.selectReviewsByPlace(selectReview);
     }
 
-    public String removeReview(int index) {
+    String removeReview(int index) {
         try {
             reviewRepository.deleteReview(index);
-        }
-        catch (Exception e){
-           return e.getMessage();
-        }
-        return "Success";
-    }
-
-    public String addReview(Review review){
-        try {
-            Review addReview = new Review(review);
-            reviewRepository.insertReview(addReview);
+            return "Success";
         }
         catch (Exception e){
             return e.getMessage();
         }
-        return "Success";
+    }
+
+    String addReview(Review review){
+        review.setUserId(SecurityContextHolder.getContext().getAuthentication().getName());
+        review.setTimeToNow();
+
+        try {
+            reviewRepository.insertReview(review);
+            return "Success";
+        }
+        catch (Exception e){
+            return e.getMessage();
+        }
     }
 }
