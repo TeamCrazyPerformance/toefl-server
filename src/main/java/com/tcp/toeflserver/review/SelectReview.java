799ac197cd557fc38fbb874de8d84f3c69c316a5
Kwoo <dlrjsdn4357@gmail.com>
2020-04-09 18:50:00 +0900

    #24 response 객체가 API 명세를 만족할 수 있도록 변경. SelectReview에서 쓰이지 않는 요소 삭제

--- src/main/java/com/tcp/toeflserver/review/SelectReview.java
+++ src/main/java/com/tcp/toeflserver/review/SelectReview.java
@@ -1,20 +1,11 @@
 package com.tcp.toeflserver.review;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 @Data
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class SelectReview {
     private String id;
     private int page;
     private String sort;
     private String order;
-
-    public SelectReview(String id, int page, String sort, String order){
-        this.id = id;
-        this.page = page;
-        this.sort = sort;
-        this.order = order;
-    }
 }
